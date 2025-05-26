4767ea3a982d8cbd714437d0134cd423cd4cdf50
adriellbezerra <adrielprof01@gmail.com>
2025-05-25 21:13:55 -0300

    Aula 15: propriedades (props) e children de componentes no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/theme.css';
+import './styles/global.css';
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
+  console.log('Oi');
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <Heading>Olá Mundo 1</Heading>
+      <Heading>Olá Mundo 2</Heading>
+      <Heading>Olá Mundo 3</Heading>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        explicabo ducimus alias iste nam praesentium tempora dignissimos quia
+        commodi cum! Architecto blanditiis adipisci provident quos laboriosam
+        qui totam expedita modi.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+//Uma forma de exportar
+//export { App };
+
+//outra forma de exportar
+//export default App;
